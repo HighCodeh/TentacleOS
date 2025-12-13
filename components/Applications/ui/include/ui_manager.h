89fef5a74973091be6c57adbae15e1e4e9692788
Emanuel Magalhaes <emanuel.mag.jr@gmail.com>
2025-12-13 00:54:16 -0300

    feat(UI): i forget to send header to use ui_manager

---
+++ components/Applications/ui/include/ui_manager.h
@@ -0,0 +1,23 @@
+#ifndef UI_MANAGER_H
+#define UI_MANAGER_H
+
+#include <stdbool.h>
+
+typedef enum {
+    SCREEN_NONE,
+    SCREEN_HOME,
+    SCREEN_MENU,
+    SCREEN_WIFI_MENU,
+    SCREEN_WIFI_SCAN,
+    SCREEN_BLE_MENU,
+    // others screens
+} screen_id_t;
+
+void ui_init(void);
+
+bool ui_acquire(void);
+void ui_release(void);
+void ui_switch_screen(screen_id_t new_screen);
+
+#endif
+
