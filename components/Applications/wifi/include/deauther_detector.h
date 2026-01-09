e65b6852451a90b2ffe158d7067e94e4b486ca2b
Emanuel Magalhaes <emanuel.mag.jr@gmail.com>
2026-01-09 00:36:09 -0300

    chore(deauther_detector): remove useless comments

--- components/Applications/wifi/include/deauther_detector.h
+++ components/Applications/wifi/include/deauther_detector.h
@@ -1,28 +1,25 @@
+// Copyright (c) 2025 HIGH CODE LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 #ifndef DEAUTHER_DETECTOR_H
 #define DEAUTHER_DETECTOR_H
 
 #include <stdint.h>
 
-/**
- * @brief Starts the deauther detector.
- * 
- * Takes the WiFi interface into promiscuous mode, starts a channel hopping task
- * (allocated in PSRAM) and counts deauthentication frames (counter in PSRAM).
- */
 void deauther_detector_start(void);
-
-/**
- * @brief Stops the deauther detector.
- * 
- * Disables promiscuous mode and deletes the channel hopping task.
- */
 void deauther_detector_stop(void);
-
-/**
- * @brief Get the current deauth packet count.
- * 
- * @return uint32_t Number of deauth packets detected.
- */
 uint32_t deauther_detector_get_count(void);
 
 #endif // DEAUTHER_DETECTOR_H
