7b8b2e6f80e5c595db7f5efc4c02fb1a96a2a4fb
luis_thiago <lthiagofarias@gmail.com>
2026-01-09 21:37:01 -0300

    feat(ui): add sound settings screen

---
+++ components/Applications/ui/screens/sound_settings/include/sound_settings_ui.h
@@ -0,0 +1,16 @@
+#ifndef SOUND_SETTINGS_UI_H
+#define SOUND_SETTINGS_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl.h"
+
+void ui_sound_settings_open(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
