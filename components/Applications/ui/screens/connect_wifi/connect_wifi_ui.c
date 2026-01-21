f9ab92d3ff71ab655676fabe9aa10044bb149449
luis_thiago <lthiagofarias@gmail.com>
2026-01-20 23:14:41 -0300

    feat(ui): keyboard in connect wifi screen

--- components/Applications/ui/screens/connect_wifi/connect_wifi_ui.c
+++ components/Applications/ui/screens/connect_wifi/connect_wifi_ui.c
@@ -1,4 +1,5 @@
 #include "connect_wifi_ui.h"
+#include "keyboard_ui.h"
 #include "header_ui.h"
 #include "footer_ui.h"
 #include "ui_theme.h"
@@ -11,8 +12,11 @@
 extern lv_group_t * main_group;
 
 static lv_obj_t * screen_wifi_list = NULL;
+static lv_obj_t * wifi_list_cont = NULL;
+static lv_obj_t * password_ta = NULL;
 static lv_style_t style_menu;
 static lv_style_t style_item;
+static lv_style_t style_ta;
 static bool styles_initialized = false;
 
 static void init_styles(void) {
@@ -22,9 +26,8 @@ static void init_styles(void) {
     lv_style_set_bg_opa(&style_menu, LV_OPA_TRANSP);
     lv_style_set_border_width(&style_menu, 2);
     lv_style_set_border_color(&style_menu, current_theme.border_accent);
-    lv_style_set_radius(&style_menu, 0); 
+    lv_style_set_radius(&style_menu, 0);
     lv_style_set_pad_all(&style_menu, 4);
-    lv_style_set_pad_row(&style_menu, 4);
 
     lv_style_init(&style_item);
     lv_style_set_bg_color(&style_item, current_theme.bg_item_bot);
@@ -33,6 +36,13 @@ static void init_styles(void) {
     lv_style_set_border_width(&style_item, 1);
     lv_style_set_border_color(&style_item, current_theme.border_inactive);
     lv_style_set_radius(&style_item, 0);
+
+    lv_style_init(&style_ta);
+    lv_style_set_bg_color(&style_ta, lv_color_black());
+    lv_style_set_border_color(&style_ta, current_theme.border_accent);
+    lv_style_set_border_width(&style_ta, 1);
+    lv_style_set_text_color(&style_ta, current_theme.text_main);
+    lv_style_set_radius(&style_ta, 0);
 
     styles_initialized = true;
 }
@@ -57,11 +67,19 @@ static void wifi_item_event_cb(lv_event_t * e) {
         if(key == LV_KEY_ESC || key == LV_KEY_LEFT) {
             buzzer_play_sound_file("buzzer_click");
             ui_switch_screen(SCREEN_CONNECTION_SETTINGS);
-            return;
         }
-
-        if(key == LV_KEY_ENTER || key == LV_KEY_RIGHT) {
+        else if(key == LV_KEY_ENTER || key == LV_KEY_RIGHT) {
             buzzer_play_sound_file("buzzer_hacker_confirm");
+
+            password_ta = lv_textarea_create(lv_screen_active());
+            lv_obj_add_style(password_ta, &style_ta, 0);
+            lv_obj_set_size(password_ta, 220, 35);
+            lv_obj_align(password_ta, LV_ALIGN_TOP_MID, 0, 35);
+            lv_textarea_set_password_mode(password_ta, true);
+            lv_textarea_set_placeholder_text(password_ta, "Password...");
+            lv_textarea_set_one_line(password_ta, true);
+
+            keyboard_open(password_ta);
         }
     }
 }
@@ -77,41 +95,38 @@ void ui_connect_wifi_open(void) {
     header_ui_create(screen_wifi_list);
     footer_ui_create(screen_wifi_list);
 
-    lv_obj_t * menu = lv_obj_create(screen_wifi_list);
-    lv_obj_set_size(menu, 230, 160);
-    lv_obj_align(menu, LV_ALIGN_CENTER, 0, 5);
-    lv_obj_add_style(menu, &style_menu, 0);
-    lv_obj_set_flex_flow(menu, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_scrollbar_mode(menu, LV_SCROLLBAR_MODE_OFF);
+    wifi_list_cont = lv_obj_create(screen_wifi_list);
+    lv_obj_set_size(wifi_list_cont, 230, 160);
+    lv_obj_align(wifi_list_cont, LV_ALIGN_CENTER, 0, 10);
+    lv_obj_add_style(wifi_list_cont, &style_menu, 0);
+    lv_obj_set_flex_flow(wifi_list_cont, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_scrollbar_mode(wifi_list_cont, LV_SCROLLBAR_MODE_OFF);
 
-    lv_obj_t * loading_label = lv_label_create(menu);
-    lv_label_set_text(loading_label, "BUSCANDO REDES...");
+    lv_obj_t * loading_label = lv_label_create(wifi_list_cont);
+    lv_label_set_text(loading_label, "SCANNING...");
     lv_obj_set_style_text_color(loading_label, current_theme.text_main, 0);
-    lv_obj_set_width(loading_label, lv_pct(100));
-    lv_obj_set_style_text_align(loading_label, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_center(loading_label);
     
     lv_screen_load(screen_wifi_list);
     lv_refr_now(NULL); 
 
     wifi_start(); 
-    vTaskDelay(pdMS_TO_TICKS(100)); 
     wifi_service_scan(); 
     lv_obj_del(loading_label);
 
     uint16_t ap_count = wifi_service_get_ap_count();
 
     if (ap_count == 0) {
-        lv_obj_t * empty_label = lv_label_create(menu);
-        lv_label_set_text(empty_label, "NENHUMA REDE");
-        lv_obj_set_style_text_color(empty_label, current_theme.text_main, 0);
-        lv_obj_set_width(empty_label, lv_pct(100));
-        lv_obj_set_style_text_align(empty_label, LV_TEXT_ALIGN_CENTER, 0);
+        lv_obj_t * empty = lv_label_create(wifi_list_cont);
+        lv_label_set_text(empty, "NO NETWORKS");
+        lv_obj_set_style_text_color(empty, current_theme.text_main, 0);
+        lv_obj_center(empty);
     } else {
         for(int i = 0; i < ap_count; i++) {
             wifi_ap_record_t * ap = wifi_service_get_ap_record(i);
             if(!ap) continue;
 
-            lv_obj_t * item = lv_obj_create(menu);
+            lv_obj_t * item = lv_obj_create(wifi_list_cont);
             lv_obj_set_size(item, lv_pct(100), 40);
             lv_obj_add_style(item, &style_item, 0);
             lv_obj_set_flex_flow(item, LV_FLEX_FLOW_ROW);
@@ -130,15 +145,14 @@ void ui_connect_wifi_open(void) {
 
             if(ap->authmode != WIFI_AUTH_OPEN) {
                 lv_obj_t * lock = lv_label_create(item);
-                lv_label_set_text(lock, LV_SYMBOL_SETTINGS); 
+                lv_label_set_text(lock, LV_SYMBOL_DIRECTORY); 
                 lv_obj_set_style_text_color(lock, current_theme.text_main, 0); 
             }
 
+            lv_obj_set_user_data(item, (void*)ap->ssid);
             lv_obj_add_event_cb(item, wifi_item_event_cb, LV_EVENT_ALL, NULL);
             
-            if(main_group) {
-                lv_group_add_obj(main_group, item);
-            }
+            if(main_group) lv_group_add_obj(main_group, item);
         }
     }
 }
