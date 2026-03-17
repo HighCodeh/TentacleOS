c7fdee91c0e2185a3acdf1e8a2d5eebac3764959
Emanuel Magalhaes <emanuel.mag.jr@gmail.com>
2026-03-17 12:26:26 -0300

    chore(main): verify ota post boot before kernel_init

--- firmware_p4/main/main.c
+++ firmware_p4/main/main.c
@@ -2,9 +2,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "kernel.h"
+#include "ota_service.h"
 #include "ui_manager.h"
 
 
 void app_main(void) {
+  ota_post_boot_check();
   kernel_init();
 }
