cc0a0255b92f4997c4f9d6193822aaf4e1c9d06b
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-03-19 00:03:58 -0300

    feat(st25r3916): add core driver header with init and field control API

--- firmware_p4/components/Drivers/st25r3916/include/st25r3916_core.h
+++ firmware_p4/components/Drivers/st25r3916/include/st25r3916_core.h
@@ -1,17 +1,7 @@
-// Copyright (c) 2025 HIGH CODE LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/**
+ * @file st25r3916_core.h
+ * @brief ST25R3916 Core init, reset, field, mode.
+ */
 #ifndef ST25R3916_CORE_H
 #define ST25R3916_CORE_H
 
@@ -28,6 +18,7 @@ bool         st25r_field_is_on(void);
 hb_nfc_err_t st25r_field_cycle(void);
 hb_nfc_err_t st25r_set_mode_nfca(void);
 
+/** Dump all 64 registers to log. */
 void st25r_dump_regs(void);
 
 #endif
