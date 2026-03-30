da6a7e8fa2d682b4aefc8fe42d61d431bcb3c215
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-03-30 12:28:36 -0300

    feat(ui): new intensity bar component

---
+++ firmware_p4/components/Applications/ui/components/intensity_bar/include/intensity_bar_ui.h
@@ -0,0 +1,20 @@
+#ifndef UI_INTENSITY_BAR_H
+#define UI_INTENSITY_BAR_H
+
+#include "lvgl.h"
+
+#define INTENSITY_BAR_STEPS 5
+
+typedef struct {
+    lv_obj_t * obj;
+    lv_obj_t * bars[INTENSITY_BAR_STEPS];
+    int level;
+} intensity_bar_t;
+
+void intensity_bar_create(intensity_bar_t * ib, lv_obj_t * parent);
+void intensity_bar_set(intensity_bar_t * ib, int level);
+int  intensity_bar_get(intensity_bar_t * ib);
+void intensity_bar_inc(intensity_bar_t * ib);
+void intensity_bar_dec(intensity_bar_t * ib);
+
+#endif
