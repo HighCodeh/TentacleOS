8ebf8c3ceb8007816ab58e78f4198ba537241332
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-03-30 12:28:03 -0300

    feat(ui): new toggle button component

---
+++ firmware_p4/components/Applications/ui/components/toggle/include/toggle_ui.h
@@ -0,0 +1,18 @@
+#ifndef UI_TOGGLE_H
+#define UI_TOGGLE_H
+
+#include "lvgl.h"
+#include <stdbool.h>
+
+typedef struct {
+    lv_obj_t * obj;
+    lv_obj_t * dot;
+    bool state;
+} toggle_ui_t;
+
+void toggle_ui_create(toggle_ui_t * toggle, lv_obj_t * parent);
+void toggle_ui_set(toggle_ui_t * toggle, bool on);
+void toggle_ui_toggle(toggle_ui_t * toggle);
+bool toggle_ui_get(toggle_ui_t * toggle);
+
+#endif
