4bd1696ffe5f3fd565c023e4a27e34ba4ad7fb9c
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-08-04 18:14:13 -0300

    refactor(ui): redesign send and burst transmit screens

--- firmware_p4/components/Applications/ui/screens/infrared/ir_burst_ui.c
+++ firmware_p4/components/Applications/ui/screens/infrared/ir_burst_ui.c
@@ -19,47 +19,121 @@
 #include "lvgl.h"
 
 #include "buttons_gpio.h"
+#include "notify_ui.h"
+#include "sigwave_ui.h"
 #include "ui_chrome.h"
+#include "ui_feedback.h"
 #include "ui_manager.h"
 #include "ui_theme.h"
-#include "waves_ui.h"
 
 static const char *TAG = "IR_BURST_UI";
 
 #define SIG_GREEN 0x00E676
+#define COL_DIM   0x8A8594
+#define BAR_TRACK 0x202028
 
-#define HEADER_TITLE_Y     10
-#define HEADER_RULE_Y      32
-#define HEADER_RULE_W      70
-#define HEADER_RULE_H      2
-#define HEADER_RULE_RADIUS 1
+#define IR_BURST_ICON "/assets/icons/bolt.bin"
 
 #define STATUS_Y 48
-#define COUNT_Y  66
-#define WAVES_Y  8
+#define COUNT_Y  72
+#define BAR_Y    94
+#define BAR_W    200
+#define BAR_H    8
+
+#define CARD_W       200
+#define CARD_H       86
+#define CARD_RADIUS  13
+#define CARD_Y_OFS   16
+#define CARD_RISE_PX 26
+#define CARD_RISE_MS 300
+
+#define LOG_Y -30
 
 #define NAV_TIMER_INTERVAL_MS 50
 #define BURST_TICK_MS         180
 #define BURST_TOTAL           24
 
-#define STATUS_BUSY "Burst running..."
+#define STATUS_BUSY "Bursting..."
 #define STATUS_DONE "Burst complete!"
 #define HINT_BUSY   "BACK to cancel"
 #define HINT_DONE   "BACK = Exit"
+
+static const char *MOCK_FILES[] = {
+    "> NEC/power       OK",
+    "> SAMSUNG/vol_up  OK",
+    "> SONY/mute       OK",
+    "> RC5/ch_up       OK",
+    "> NEC/source      OK",
+    "> LG/menu         OK",
+    "> SAMSUNG/power   OK",
+    "> SONY/vol_down   OK",
+};
+#define MOCK_FILES_N ((int)(sizeof(MOCK_FILES) / sizeof(MOCK_FILES[0])))
 
 static lv_obj_t *s_screen = NULL;
 static lv_timer_t *s_nav_timer = NULL;
 static lv_timer_t *s_burst_timer = NULL;
 static lv_obj_t *s_status_label = NULL;
 static lv_obj_t *s_count_label = NULL;
+static lv_obj_t *s_bar = NULL;
+static lv_obj_t *s_card = NULL;
+static lv_obj_t *s_caption = NULL;
+static lv_obj_t *s_sig = NULL;
+static lv_obj_t *s_log_label = NULL;
 static lv_obj_t *s_footer = NULL;
-static lv_obj_t *s_waves = NULL;
 static int s_sent = 0;
 
 static bool s_btn_back_last = false;
 
 static void nav_timer_cb(lv_timer_t *timer);
 static void burst_tick_cb(lv_timer_t *timer);
+
+static void transy_cb(void *var, int32_t v) {
+  lv_obj_set_style_translate_y((lv_obj_t *)var, v, 0);
+}
+
+static void opa_cb(void *var, int32_t v) {
+  lv_obj_set_style_opa((lv_obj_t *)var, (lv_opa_t)v, 0);
+}
+
+static lv_obj_t *lit_panel(lv_obj_t *parent, int w, int h) {
+  lv_obj_t *p = lv_obj_create(parent);
+  lv_obj_remove_flag(p, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_size(p, w, h);
+  lv_obj_set_style_radius(p, CARD_RADIUS, 0);
+  lv_obj_set_style_bg_color(p, current_theme.bg_secondary, 0);
+  lv_obj_set_style_bg_grad_dir(p, LV_GRAD_DIR_NONE, 0);
+  lv_obj_set_style_bg_opa(p, LV_OPA_COVER, 0);
+  lv_obj_set_style_border_width(p, 1, 0);
+  lv_obj_set_style_border_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_border_opa(p, LV_OPA_COVER, 0);
+  lv_obj_set_style_shadow_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_shadow_width(p, 16, 0);
+  lv_obj_set_style_shadow_opa(p, LV_OPA_40, 0);
+  lv_obj_set_style_shadow_spread(p, -4, 0);
+  lv_obj_set_style_pad_all(p, 8, 0);
+  return p;
+}
+
+static void card_rise(lv_obj_t *obj) {
+  lv_anim_t a;
+  lv_anim_init(&a);
+  lv_anim_set_var(&a, obj);
+  lv_anim_set_exec_cb(&a, transy_cb);
+  lv_anim_set_values(&a, CARD_RISE_PX, 0);
+  lv_anim_set_duration(&a, CARD_RISE_MS);
+  lv_anim_set_path_cb(&a, lv_anim_path_ease_out);
+  lv_anim_start(&a);
+
+  lv_anim_t f;
+  lv_anim_init(&f);
+  lv_anim_set_var(&f, obj);
+  lv_anim_set_exec_cb(&f, opa_cb);
+  lv_anim_set_values(&f, LV_OPA_TRANSP, LV_OPA_COVER);
+  lv_anim_set_duration(&f, 280);
+  lv_anim_set_path_cb(&f, lv_anim_path_ease_out);
+  lv_anim_start(&f);
+}
 
 void ui_ir_burst_open(void) {
   if (s_screen != NULL) {
@@ -68,6 +142,10 @@ void ui_ir_burst_open(void) {
   }
   s_sent = 0;
   s_burst_timer = NULL;
+  s_card = NULL;
+  s_sig = NULL;
+  s_caption = NULL;
+  s_btn_back_last = false;
 
   s_screen = lv_obj_create(NULL);
   lv_obj_set_style_bg_color(s_screen, current_theme.screen_base, 0);
@@ -76,7 +154,7 @@ void ui_ir_burst_open(void) {
   lv_obj_set_style_border_width(s_screen, 0, 0);
   lv_obj_set_style_pad_all(s_screen, 0, 0);
 
-  ui_chrome_header(s_screen, "IR BURST", "/assets/icons/burst_menu_icon.bin");
+  ui_chrome_header(s_screen, "IR BURST", IR_BURST_ICON);
 
   s_status_label = lv_label_create(s_screen);
   lv_label_set_text(s_status_label, STATUS_BUSY);
@@ -87,12 +165,42 @@ void ui_ir_burst_open(void) {
 
   s_count_label = lv_label_create(s_screen);
   lv_label_set_text_fmt(s_count_label, "0 / %d files", BURST_TOTAL);
-  lv_obj_set_style_text_color(s_count_label, current_theme.border_accent, 0);
+  lv_obj_set_style_text_color(s_count_label, lv_color_hex(COL_DIM), 0);
   lv_obj_set_style_text_font(s_count_label, &lv_font_montserrat_12, 0);
   lv_obj_set_style_text_align(s_count_label, LV_TEXT_ALIGN_CENTER, 0);
   lv_obj_align(s_count_label, LV_ALIGN_TOP_MID, 0, COUNT_Y);
 
-  s_waves = waves_create(s_screen, LV_ALIGN_CENTER, 0, WAVES_Y, LV_SYMBOL_UPLOAD, NULL);
+  s_bar = lv_bar_create(s_screen);
+  lv_obj_set_size(s_bar, BAR_W, BAR_H);
+  lv_obj_align(s_bar, LV_ALIGN_TOP_MID, 0, BAR_Y);
+  lv_obj_set_style_radius(s_bar, 4, LV_PART_MAIN);
+  lv_obj_set_style_bg_color(s_bar, lv_color_hex(BAR_TRACK), LV_PART_MAIN);
+  lv_obj_set_style_bg_grad_dir(s_bar, LV_GRAD_DIR_NONE, LV_PART_MAIN);
+  lv_obj_set_style_bg_opa(s_bar, LV_OPA_COVER, LV_PART_MAIN);
+  lv_obj_set_style_radius(s_bar, 4, LV_PART_INDICATOR);
+  lv_obj_set_style_bg_color(s_bar, current_theme.border_accent, LV_PART_INDICATOR);
+  lv_obj_set_style_bg_grad_dir(s_bar, LV_GRAD_DIR_NONE, LV_PART_INDICATOR);
+  lv_obj_set_style_bg_opa(s_bar, LV_OPA_COVER, LV_PART_INDICATOR);
+  lv_bar_set_range(s_bar, 0, BURST_TOTAL);
+  lv_bar_set_value(s_bar, 0, LV_ANIM_OFF);
+
+  s_card = lit_panel(s_screen, CARD_W, CARD_H);
+  lv_obj_align(s_card, LV_ALIGN_CENTER, 0, CARD_Y_OFS);
+
+  s_caption = lv_label_create(s_card);
+  lv_label_set_text(s_caption, "IR emitter  38 kHz");
+  lv_obj_set_style_text_color(s_caption, current_theme.border_accent, 0);
+  lv_obj_set_style_text_font(s_caption, &lv_font_montserrat_12, 0);
+  lv_obj_align(s_caption, LV_ALIGN_TOP_MID, 0, 0);
+
+  s_sig = sigwave_create(s_card, LV_ALIGN_BOTTOM_MID, 0, -2);
+
+  s_log_label = lv_label_create(s_screen);
+  lv_label_set_text(s_log_label, MOCK_FILES[0]);
+  lv_obj_set_style_text_color(s_log_label, lv_color_hex(COL_DIM), 0);
+  lv_obj_set_style_text_font(s_log_label, &lv_font_montserrat_12, 0);
+  lv_obj_set_style_text_align(s_log_label, LV_TEXT_ALIGN_CENTER, 0);
+  lv_obj_align(s_log_label, LV_ALIGN_BOTTOM_MID, 0, LOG_Y);
 
   s_footer = ui_chrome_footer(s_screen, HINT_BUSY);
 
@@ -113,15 +221,36 @@ static void burst_tick_cb(lv_timer_t *timer) {
 
   s_sent++;
   lv_label_set_text_fmt(s_count_label, "%d / %d files", s_sent, BURST_TOTAL);
+  if (s_bar)
+    lv_bar_set_value(s_bar, s_sent, LV_ANIM_OFF);
+  if (s_log_label)
+    lv_label_set_text(s_log_label, MOCK_FILES[(s_sent - 1) % MOCK_FILES_N]);
 
   if (s_sent >= BURST_TOTAL) {
     lv_label_set_text(s_status_label, STATUS_DONE);
     lv_obj_set_style_text_color(s_status_label, lv_color_hex(SIG_GREEN), 0);
-    lv_label_set_text_fmt(s_count_label, "%d signals sent", s_sent);
+    lv_label_set_text_fmt(s_count_label, "%d / %d files", BURST_TOTAL, BURST_TOTAL);
     if (s_footer)
       ui_chrome_footer_set_text(s_footer, HINT_DONE);
-    if (s_waves)
-      lv_obj_add_flag(s_waves, LV_OBJ_FLAG_HIDDEN);
+    if (s_log_label)
+      lv_obj_add_flag(s_log_label, LV_OBJ_FLAG_HIDDEN);
+
+    if (s_sig) {
+      lv_obj_del(s_sig);
+      s_sig = sigwave_create_static(s_card, LV_ALIGN_BOTTOM_MID, 0, -2);
+    }
+    if (s_caption) {
+      lv_label_set_text_fmt(s_caption, "%d signals sent", BURST_TOTAL);
+      lv_obj_set_style_text_color(s_caption, current_theme.text_main, 0);
+      lv_obj_set_style_text_font(s_caption, &lv_font_montserrat_14, 0);
+    }
+    if (s_card)
+      card_rise(s_card);
+
+    ESP_LOGI(TAG, "mock burst complete: %d signals", BURST_TOTAL);
+    ui_feedback(UI_FB_WRITE);
+    notify(NOTIFY_SAVED, "Burst sent");
+
     lv_timer_delete(timer);
     s_burst_timer = NULL;
   }
--- firmware_p4/components/Applications/ui/screens/infrared/ir_send_ui.c
+++ firmware_p4/components/Applications/ui/screens/infrared/ir_send_ui.c
@@ -18,41 +18,42 @@
 #include <stdio.h>
 
 #include "esp_log.h"
+#include "lvgl.h"
 
+#include "assets_manager.h"
 #include "buttons_gpio.h"
+#include "capture_result_ui.h"
 #include "menu_component_ui.h"
+#include "notify_ui.h"
 #include "sigwave_ui.h"
 #include "ui_chrome.h"
 #include "ui_feedback.h"
 #include "ui_manager.h"
 #include "ui_theme.h"
-#include "waves_ui.h"
 
 static const char *TAG = "IR_SEND_UI";
 
 #define SIG_GREEN 0x00E676
-
-#define HEADER_TITLE_Y     10
-#define HEADER_RULE_Y      32
-#define HEADER_RULE_W      70
-#define HEADER_RULE_H      2
-#define HEADER_RULE_RADIUS 1
-
-#define STATUS_Y   50
-#define DETAIL_Y   68
-#define HINT_Y_OFS -6
+#define COL_DIM   0x8A8594
+#define IR_ICON   "/assets/icons/podcasts.bin"
 
 #define NAV_TIMER_MS 50
 #define SENDING_MS   1600
 #define DOT_CYCLE_MS 350
+#define REVEAL_MS    2600
 
-#define IR_ICON "/assets/icons/ir_icon.bin"
+#define STATUS_Y   50
+#define CARD_W     210
+#define CARD_H     64
+#define CARD_Y_OFS -30
+#define SIG_Y_OFS  44
 
 #define STATUS_SENDING "Sending"
 #define STATUS_SENT    "Signal sent!"
 
 #define HINT_SENDING "Transmitting..."
-#define HINT_SENT    "RIGHT = Resend   BACK = Exit"
+#define HINT_SENT    "BACK = Exit"
+#define HINT_OPTIONS "UP/DOWN choose   OK do   BACK exit"
 
 static const char *MOCK_SIGNALS[] = {
     "[NEC] TV Power",
@@ -80,7 +81,13 @@ static lv_timer_t *s_send_timer = NULL;
 
 static lv_obj_t *s_status_label = NULL;
 static lv_obj_t *s_hint_label = NULL;
+static lv_obj_t *s_card = NULL;
+static lv_obj_t *s_sig = NULL;
+static capture_result_t s_cr = {0};
+static bool s_options = false;
+static bool s_saved = false;
 static uint32_t s_send_start = 0;
+static uint32_t s_sent_at = 0;
 
 static bool s_btn_up_last = false;
 static bool s_btn_down_last = false;
@@ -120,32 +127,36 @@ static lv_obj_t *new_screen(void) {
   return screen;
 }
 
-static void build_header(const char *text) {
-  lv_obj_t *title = lv_label_create(s_screen);
-  lv_label_set_text(title, text);
-  lv_obj_set_style_text_color(title, current_theme.border_accent, 0);
-  lv_obj_set_style_text_font(title, &lv_font_montserrat_14, 0);
-  lv_obj_align(title, LV_ALIGN_TOP_MID, 0, HEADER_TITLE_Y);
-
-  lv_obj_t *rule = lv_obj_create(s_screen);
-  lv_obj_remove_flag(rule, LV_OBJ_FLAG_SCROLLABLE);
-  lv_obj_remove_flag(rule, LV_OBJ_FLAG_CLICKABLE);
-  lv_obj_set_size(rule, lv_pct(HEADER_RULE_W), HEADER_RULE_H);
-  lv_obj_align(rule, LV_ALIGN_TOP_MID, 0, HEADER_RULE_Y);
-  lv_obj_set_style_border_width(rule, 0, 0);
-  lv_obj_set_style_radius(rule, HEADER_RULE_RADIUS, 0);
-  lv_obj_set_style_bg_color(rule, current_theme.border_accent, 0);
-  lv_obj_set_style_bg_opa(rule, LV_OPA_40, 0);
+static void transy_cb(void *var, int32_t v) {
+  lv_obj_set_style_translate_y((lv_obj_t *)var, v, 0);
 }
 
-static lv_obj_t *make_hint(const char *text) {
-  lv_obj_t *hint = lv_label_create(s_screen);
-  lv_label_set_text(hint, text);
-  lv_obj_set_style_text_color(hint, current_theme.text_main, 0);
-  lv_obj_set_style_text_opa(hint, LV_OPA_60, 0);
-  lv_obj_set_style_text_font(hint, &lv_font_montserrat_12, 0);
-  lv_obj_align(hint, LV_ALIGN_BOTTOM_MID, 0, HINT_Y_OFS);
-  return hint;
+static void card_rise(lv_obj_t *o) {
+  lv_anim_t a;
+  lv_anim_init(&a);
+  lv_anim_set_var(&a, o);
+  lv_anim_set_exec_cb(&a, transy_cb);
+  lv_anim_set_values(&a, 26, 0);
+  lv_anim_set_duration(&a, 300);
+  lv_anim_set_path_cb(&a, lv_anim_path_ease_out);
+  lv_anim_start(&a);
+}
+
+static lv_obj_t *lit_panel(lv_obj_t *parent, int w, int h) {
+  lv_obj_t *p = lv_obj_create(parent);
+  lv_obj_remove_flag(p, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_size(p, w, h);
+  lv_obj_set_style_radius(p, 13, 0);
+  lv_obj_set_style_bg_color(p, current_theme.bg_secondary, 0);
+  lv_obj_set_style_bg_opa(p, LV_OPA_COVER, 0);
+  lv_obj_set_style_bg_grad_dir(p, LV_GRAD_DIR_NONE, 0);
+  lv_obj_set_style_border_width(p, 1, 0);
+  lv_obj_set_style_border_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_shadow_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_shadow_width(p, 16, 0);
+  lv_obj_set_style_shadow_opa(p, LV_OPA_40, 0);
+  lv_obj_set_style_shadow_spread(p, -4, 0);
+  return p;
 }
 
 static void set_status(const char *text, bool success) {
@@ -156,6 +167,64 @@ static void set_status(const char *text, bool success) {
       s_status_label, success ? lv_color_hex(SIG_GREEN) : current_theme.text_main, 0);
 }
 
+static void set_hint(const char *text) {
+  if (s_hint_label != NULL)
+    ui_chrome_footer_set_text(s_hint_label, text);
+}
+
+static lv_obj_t *build_cartridge(lv_obj_t *parent) {
+  lv_obj_t *card = lit_panel(parent, CARD_W, CARD_H);
+  lv_obj_align(card, LV_ALIGN_CENTER, 0, CARD_Y_OFS);
+  lv_obj_set_style_pad_all(card, 10, 0);
+  lv_obj_set_flex_flow(card, LV_FLEX_FLOW_ROW);
+  lv_obj_set_flex_align(card, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+  lv_obj_set_style_pad_column(card, 10, 0);
+
+  lv_obj_t *well = lv_obj_create(card);
+  lv_obj_remove_flag(well, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_size(well, 40, 40);
+  lv_obj_set_style_radius(well, 10, 0);
+  lv_obj_set_style_bg_color(well, current_theme.bg_primary, 0);
+  lv_obj_set_style_bg_opa(well, LV_OPA_COVER, 0);
+  lv_obj_set_style_bg_grad_dir(well, LV_GRAD_DIR_NONE, 0);
+  lv_obj_set_style_border_width(well, 0, 0);
+  lv_obj_set_style_pad_all(well, 0, 0);
+
+  lv_image_dsc_t *dsc = assets_get(IR_ICON);
+  if (dsc != NULL) {
+    lv_obj_t *img = lv_image_create(well);
+    lv_image_set_src(img, dsc);
+    lv_obj_center(img);
+    lv_obj_set_style_image_recolor(img, current_theme.text_main, 0);
+    lv_obj_set_style_image_recolor_opa(img, LV_OPA_COVER, 0);
+  }
+
+  lv_obj_t *col = lv_obj_create(card);
+  lv_obj_remove_flag(col, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_height(col, LV_SIZE_CONTENT);
+  lv_obj_set_flex_grow(col, 1);
+  lv_obj_set_style_bg_opa(col, LV_OPA_TRANSP, 0);
+  lv_obj_set_style_border_width(col, 0, 0);
+  lv_obj_set_style_pad_all(col, 0, 0);
+  lv_obj_set_flex_flow(col, LV_FLEX_FLOW_COLUMN);
+  lv_obj_set_flex_align(col, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
+  lv_obj_set_style_pad_row(col, 3, 0);
+
+  lv_obj_t *name = lv_label_create(col);
+  lv_label_set_long_mode(name, LV_LABEL_LONG_DOT);
+  lv_obj_set_width(name, lv_pct(100));
+  lv_label_set_text(name, MOCK_SIGNALS[s_sel]);
+  lv_obj_set_style_text_font(name, &lv_font_montserrat_14, 0);
+  lv_obj_set_style_text_color(name, current_theme.text_main, 0);
+
+  lv_obj_t *sub = lv_label_create(col);
+  lv_label_set_text(sub, "Transmitting 38kHz");
+  lv_obj_set_style_text_font(sub, &lv_font_montserrat_12, 0);
+  lv_obj_set_style_text_color(sub, current_theme.border_accent, 0);
+
+  return card;
+}
+
 static void build_list(void) {
   if (s_screen != NULL) {
     lv_obj_del(s_screen);
@@ -163,15 +232,19 @@ static void build_list(void) {
   }
   s_status_label = NULL;
   s_hint_label = NULL;
+  s_card = NULL;
+  s_sig = NULL;
+  s_cr = (capture_result_t){0};
+  s_options = false;
 
   s_screen = lv_obj_create(NULL);
   lv_obj_set_style_bg_color(s_screen, current_theme.screen_base, 0);
   lv_obj_set_style_bg_opa(s_screen, LV_OPA_COVER, 0);
   lv_obj_remove_flag(s_screen, LV_OBJ_FLAG_SCROLLABLE);
 
-  s_menu = menu_component_create(s_screen, "IR SEND", "/assets/icons/ir_send_menu_icon.bin");
+  s_menu = menu_component_create(s_screen, "IR SEND", IR_ICON);
   for (int i = 0; i < MOCK_SIGNALS_COUNT; i++)
-    menu_component_add_item(&s_menu, IR_ICON, MOCK_SIGNALS[i]);
+    menu_component_add_item(&s_menu, "/assets/icons/graphic_eq.bin", MOCK_SIGNALS[i]);
   if (s_sel > 0 && s_sel < MOCK_SIGNALS_COUNT)
     menu_component_select(&s_menu, s_sel);
 
@@ -183,9 +256,11 @@ static void build_sending(void) {
     lv_obj_del(s_screen);
     s_screen = NULL;
   }
+  s_cr = (capture_result_t){0};
+  s_options = false;
 
   s_screen = new_screen();
-  ui_chrome_header(s_screen, "Send", "/assets/icons/ir_send_menu_icon.bin");
+  ui_chrome_header(s_screen, "Send", IR_ICON);
 
   s_status_label = lv_label_create(s_screen);
   lv_label_set_text(s_status_label, STATUS_SENDING);
@@ -194,48 +269,40 @@ static void build_sending(void) {
   lv_obj_set_style_text_align(s_status_label, LV_TEXT_ALIGN_CENTER, 0);
   lv_obj_align(s_status_label, LV_ALIGN_TOP_MID, 0, STATUS_Y);
 
-  lv_obj_t *which = lv_label_create(s_screen);
-  lv_label_set_text(which, MOCK_SIGNALS[s_sel]);
-  lv_obj_set_style_text_color(which, current_theme.border_accent, 0);
-  lv_obj_set_style_text_font(which, &lv_font_montserrat_12, 0);
-  lv_obj_set_style_text_align(which, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_align(which, LV_ALIGN_TOP_MID, 0, DETAIL_Y);
-
-  waves_create(s_screen, LV_ALIGN_CENTER, 0, 6, NULL, IR_ICON);
-  sigwave_create(s_screen, LV_ALIGN_BOTTOM_MID, 0, -28);
+  s_card = build_cartridge(s_screen);
+  s_sig = sigwave_create(s_screen, LV_ALIGN_CENTER, 0, SIG_Y_OFS);
 
   s_hint_label = ui_chrome_footer(s_screen, HINT_SENDING);
 
   ui_screen_load(s_screen);
 }
 
-static void show_sent(void) {
-  if (s_screen != NULL) {
-    lv_obj_del(s_screen);
-    s_screen = NULL;
+static void show_options(void) {
+  if (s_card != NULL) {
+    lv_obj_del(s_card);
+    s_card = NULL;
+  }
+  if (s_sig != NULL) {
+    lv_obj_del(s_sig);
+    s_sig = NULL;
+  }
+  if (s_status_label != NULL) {
+    lv_obj_add_flag(s_status_label, LV_OBJ_FLAG_HIDDEN);
   }
 
-  s_screen = new_screen();
-  ui_chrome_header(s_screen, "Send", "/assets/icons/ir_send_menu_icon.bin");
-
-  waves_create(s_screen, LV_ALIGN_CENTER, 0, 6, LV_SYMBOL_OK, NULL);
-
-  s_status_label = lv_label_create(s_screen);
-  lv_obj_set_style_text_font(s_status_label, &lv_font_montserrat_14, 0);
-  lv_obj_set_style_text_align(s_status_label, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_align(s_status_label, LV_ALIGN_TOP_MID, 0, STATUS_Y);
-  set_status(STATUS_SENT, true);
-
-  lv_obj_t *which = lv_label_create(s_screen);
-  lv_label_set_text(which, MOCK_SIGNALS[s_sel]);
-  lv_obj_set_style_text_color(which, current_theme.border_accent, 0);
-  lv_obj_set_style_text_font(which, &lv_font_montserrat_12, 0);
-  lv_obj_set_style_text_align(which, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_align(which, LV_ALIGN_TOP_MID, 0, DETAIL_Y);
-
-  s_hint_label = ui_chrome_footer(s_screen, HINT_SENT);
-
-  ui_screen_load(s_screen);
+  capture_result_cfg_t cfg = {
+      .accent = current_theme.border_accent,
+      .card_icon = IR_ICON,
+      .card_title = MOCK_SIGNALS[s_sel],
+      .card_sub = "NEC",
+      .card_value = "sent",
+      .primary_label = "Send again",
+      .again_label = "Pick another",
+  };
+  s_cr = capture_result_create(s_screen, &cfg);
+  s_options = true;
+  s_saved = false;
+  set_hint(HINT_OPTIONS);
 }
 
 static void start_send(void) {
@@ -244,6 +311,7 @@ static void start_send(void) {
   s_send_start = lv_tick_get();
   ESP_LOGI(TAG, "mock send: %s", MOCK_SIGNALS[s_sel]);
   build_sending();
+  ui_feedback(UI_FB_SELECT);
   s_send_timer = lv_timer_create(send_done_cb, SENDING_MS, NULL);
   lv_timer_set_repeat_count(s_send_timer, 1);
 }
@@ -253,9 +321,25 @@ static void send_done_cb(lv_timer_t *t) {
   s_send_timer = NULL;
   if (lv_screen_active() != s_screen)
     return;
+
   s_view = VIEW_SENT;
+  s_options = false;
+  set_status(STATUS_SENT, true);
+
+  if (s_sig != NULL) {
+    lv_obj_del(s_sig);
+    s_sig = NULL;
+  }
+  s_sig = sigwave_create_static(s_screen, LV_ALIGN_CENTER, 0, SIG_Y_OFS);
+
+  if (s_card != NULL) {
+    lv_obj_fade_in(s_card, 280, 0);
+    card_rise(s_card);
+  }
+
+  s_sent_at = lv_tick_get();
+  set_hint(HINT_SENT);
   ui_feedback(UI_FB_WRITE);
-  show_sent();
 }
 
 static void sending_tick(void) {
@@ -295,10 +379,14 @@ static void nav_timer_cb(lv_timer_t *t) {
 
   switch (s_view) {
     case VIEW_LIST:
-      if (down && !s_btn_down_last)
+      if (down && !s_btn_down_last) {
         menu_component_next(&s_menu);
-      if (up && !s_btn_up_last)
+        ui_feedback(UI_FB_NAV);
+      }
+      if (up && !s_btn_up_last) {
         menu_component_prev(&s_menu);
+        ui_feedback(UI_FB_NAV);
+      }
       if ((ok && !s_btn_ok_last) || (right && !s_btn_right_last)) {
         s_sel = menu_component_get_selected(&s_menu);
         start_send();
@@ -320,16 +408,55 @@ static void nav_timer_cb(lv_timer_t *t) {
       break;
 
     case VIEW_SENT:
-      if (right && !s_btn_right_last) {
-        start_send();
-        reset_latch();
-        return;
-      }
-      if (back && !s_btn_back_last) {
-        s_view = VIEW_LIST;
-        build_list();
-        reset_latch();
-        return;
+      if (!s_options) {
+        if (back && !s_btn_back_last) {
+          s_view = VIEW_LIST;
+          build_list();
+          reset_latch();
+          return;
+        }
+        if (lv_tick_get() - s_sent_at >= REVEAL_MS)
+          show_options();
+      } else {
+        if (down && !s_btn_down_last) {
+          capture_result_next(&s_cr);
+          ui_feedback(UI_FB_NAV);
+        }
+        if (up && !s_btn_up_last) {
+          capture_result_prev(&s_cr);
+          ui_feedback(UI_FB_NAV);
+        }
+        if (ok && !s_btn_ok_last) {
+          switch (capture_result_selected(&s_cr)) {
+            case CAP_ACT_PRIMARY:
+              start_send();
+              reset_latch();
+              return;
+            case CAP_ACT_SAVE:
+              if (!s_saved) {
+                s_saved = true;
+                capture_result_mark_saved(&s_cr);
+                ESP_LOGI(TAG, "mock signal saved: %s", MOCK_SIGNALS[s_sel]);
+                ui_feedback(UI_FB_WRITE);
+                notify(NOTIFY_SAVED, "IR signal saved");
+              }
+              break;
+            case CAP_ACT_AGAIN:
+              s_view = VIEW_LIST;
+              build_list();
+              reset_latch();
+              return;
+            case CAP_ACT_DISCARD:
+              ui_switch_screen(SCREEN_IR_MENU);
+              return;
+            default:
+              break;
+          }
+        }
+        if (back && !s_btn_back_last) {
+          ui_switch_screen(SCREEN_IR_MENU);
+          return;
+        }
       }
       break;
 
@@ -353,6 +480,11 @@ void ui_ir_send_open(void) {
   }
   s_status_label = NULL;
   s_hint_label = NULL;
+  s_card = NULL;
+  s_sig = NULL;
+  s_cr = (capture_result_t){0};
+  s_options = false;
+  s_saved = false;
   s_view = VIEW_LIST;
   s_sel = 0;
   reset_latch();
