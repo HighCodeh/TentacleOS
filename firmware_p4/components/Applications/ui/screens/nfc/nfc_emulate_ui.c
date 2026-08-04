28bc4272bfe0e3e440a612a660318bef7335265f
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-08-04 18:04:45 -0300

    feat(ui): rework NFC emulate/write/saved as card-wallet flows

--- firmware_p4/components/Applications/ui/screens/nfc/nfc_emulate_ui.c
+++ firmware_p4/components/Applications/ui/screens/nfc/nfc_emulate_ui.c
@@ -17,32 +17,213 @@
 
 #include "lvgl.h"
 
+#include "st7789.h"
+
+#include "assets_manager.h"
 #include "buttons_gpio.h"
-#include "menu_component_ui.h"
+#include "keyboard_ui.h"
+#include "msgbox_ui.h"
 #include "nfc_sim.h"
 #include "nfc_ui_common.h"
+#include "page_dots_ui.h"
 #include "ui_chrome.h"
 #include "ui_feedback.h"
 #include "ui_manager.h"
 #include "ui_theme.h"
 
 #define NAV_TIMER_MS 33
-#define CARD_ICON    "/assets/icons/card_icon.bin"
-#define FIELD_GREEN  0x00E676
+#define CARD_ICON    "/assets/icons/contactless.bin"
+#define COL_DIM      0x8A8594
+#define SIG_GREEN    0x00E676
+#define TX_DOTS      3
+#define CARD_Y_OFS   (-24)
+#define FIELD_BOX_W  210
+#define FIELD_BOX_H  122
+
+#define MAX_CARDS      10
+#define WALLET_FRONT_Y (-24)
+#define WALLET_STEP    22
+#define WALLET_TAP_Y   96
+#define BACK1_W        196
+#define BACK2_W        182
+#define BACK_H         118
 
 enum { EM_NONE, EM_RUN };
 
 static lv_obj_t *s_screen = NULL;
-static menu_component_t s_menu;
+static lv_obj_t *s_body = NULL;
+static page_dots_t s_dots;
+static int s_sel = 0;
+static int s_count = 0;
 static lv_timer_t *s_nav_timer = NULL;
 static bool s_empty = false;
 
 static lv_obj_t *s_ov = NULL;
 static nfc_ui_field_t s_ov_field;
+static nfc_sim_card_t s_card;
 static int s_em = EM_NONE;
 static uint32_t s_em_start = 0;
 
 static bool s_up_last, s_down_last, s_ok_last, s_back_last, s_left_last, s_right_last;
+
+static void opa_cb(void *var, int32_t v) {
+  lv_obj_set_style_opa((lv_obj_t *)var, (lv_opa_t)v, 0);
+}
+
+static void transy_cb(void *var, int32_t v) {
+  lv_obj_set_style_translate_y((lv_obj_t *)var, v, 0);
+}
+
+static void blink_loop(lv_obj_t *o, uint32_t period, int32_t delay) {
+  lv_anim_t a;
+  lv_anim_init(&a);
+  lv_anim_set_var(&a, o);
+  lv_anim_set_exec_cb(&a, opa_cb);
+  lv_anim_set_values(&a, 255, 110);
+  lv_anim_set_duration(&a, period / 2);
+  lv_anim_set_playback_duration(&a, period / 2);
+  lv_anim_set_delay(&a, delay);
+  lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+  lv_anim_start(&a);
+}
+
+static void card_rise(lv_obj_t *o) {
+  lv_anim_t a;
+  lv_anim_init(&a);
+  lv_anim_set_var(&a, o);
+  lv_anim_set_exec_cb(&a, transy_cb);
+  lv_anim_set_values(&a, 26, 0);
+  lv_anim_set_duration(&a, 300);
+  lv_anim_set_path_cb(&a, lv_anim_path_ease_out);
+  lv_anim_start(&a);
+}
+
+static lv_obj_t *lit_panel(lv_obj_t *parent, int w, int h) {
+  lv_obj_t *p = lv_obj_create(parent);
+  lv_obj_remove_flag(p, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_size(p, w, h);
+  lv_obj_set_style_radius(p, 13, 0);
+  lv_obj_set_style_bg_color(p, current_theme.bg_secondary, 0);
+  lv_obj_set_style_bg_opa(p, LV_OPA_COVER, 0);
+  lv_obj_set_style_bg_grad_dir(p, LV_GRAD_DIR_NONE, 0);
+  lv_obj_set_style_border_width(p, 1, 0);
+  lv_obj_set_style_border_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_shadow_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_shadow_width(p, 16, 0);
+  lv_obj_set_style_shadow_opa(p, LV_OPA_40, 0);
+  lv_obj_set_style_shadow_spread(p, -4, 0);
+  return p;
+}
+
+static void build_empty(const char *icon, const char *title, const char *sub) {
+  lv_obj_t *card = lit_panel(s_screen, 200, 104);
+  lv_obj_align(card, LV_ALIGN_CENTER, 0, 6);
+  lv_obj_set_style_pad_all(card, 10, 0);
+  lv_obj_set_flex_flow(card, LV_FLEX_FLOW_COLUMN);
+  lv_obj_set_flex_align(card, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+  lv_obj_set_style_pad_row(card, 6, 0);
+
+  lv_image_dsc_t *dsc = assets_get(icon);
+  if (dsc != NULL) {
+    lv_obj_t *img = lv_image_create(card);
+    lv_image_set_src(img, dsc);
+    lv_obj_set_style_image_recolor(img, current_theme.text_main, 0);
+    lv_obj_set_style_image_recolor_opa(img, LV_OPA_COVER, 0);
+  }
+
+  lv_obj_t *t = lv_label_create(card);
+  lv_label_set_text(t, title);
+  lv_obj_set_style_text_font(t, &lv_font_montserrat_14, 0);
+  lv_obj_set_style_text_color(t, current_theme.text_main, 0);
+
+  lv_obj_t *s = lv_label_create(card);
+  lv_label_set_text(s, sub);
+  lv_obj_set_style_text_font(s, &lv_font_montserrat_12, 0);
+  lv_obj_set_style_text_color(s, lv_color_hex(COL_DIM), 0);
+}
+
+static lv_obj_t *
+wallet_back(lv_obj_t *parent, const nfc_sim_card_t *c, int w, int y_ofs, lv_opa_t opa) {
+  lv_color_t edge = nfc_ui_card_color(c);
+  lv_obj_t *b = lv_obj_create(parent);
+  lv_obj_remove_flag(b, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_remove_flag(b, LV_OBJ_FLAG_CLICKABLE);
+  lv_obj_set_size(b, w, BACK_H);
+  lv_obj_set_style_radius(b, 14, 0);
+  lv_obj_set_style_bg_color(b, current_theme.bg_secondary, 0);
+  lv_obj_set_style_bg_opa(b, LV_OPA_COVER, 0);
+  lv_obj_set_style_border_width(b, 1, 0);
+  lv_obj_set_style_border_color(b, edge, 0);
+  lv_obj_set_style_shadow_color(b, edge, 0);
+  lv_obj_set_style_shadow_width(b, 12, 0);
+  lv_obj_set_style_shadow_opa(b, LV_OPA_30, 0);
+  lv_obj_set_style_shadow_spread(b, -4, 0);
+  lv_obj_set_style_opa(b, opa, 0);
+  lv_obj_align(b, LV_ALIGN_CENTER, 0, y_ofs);
+  return b;
+}
+
+static void wallet_build(void) {
+  if (s_body != NULL) {
+    lv_obj_del(s_body);
+    s_body = NULL;
+  }
+
+  s_body = lv_obj_create(s_screen);
+  lv_obj_remove_flag(s_body, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_remove_flag(s_body, LV_OBJ_FLAG_CLICKABLE);
+  lv_obj_set_size(s_body, lv_pct(100), LCD_V_RES - UI_CHROME_HEADER_H - UI_CHROME_FOOTER_H);
+  lv_obj_align(s_body, LV_ALIGN_TOP_MID, 0, UI_CHROME_HEADER_H);
+  lv_obj_set_style_bg_opa(s_body, LV_OPA_TRANSP, 0);
+  lv_obj_set_style_border_width(s_body, 0, 0);
+  lv_obj_set_style_pad_all(s_body, 0, 0);
+
+  if (s_count >= 3)
+    wallet_back(s_body,
+                nfc_sim_saved_get((s_sel + 2) % s_count),
+                BACK2_W,
+                WALLET_FRONT_Y + 2 * WALLET_STEP,
+                LV_OPA_40);
+  if (s_count >= 2)
+    wallet_back(s_body,
+                nfc_sim_saved_get((s_sel + 1) % s_count),
+                BACK1_W,
+                WALLET_FRONT_Y + WALLET_STEP,
+                LV_OPA_70);
+
+  lv_obj_t *front = nfc_ui_card_panel(s_body, nfc_sim_saved_get(s_sel));
+  lv_obj_align(front, LV_ALIGN_CENTER, 0, WALLET_FRONT_Y);
+  card_rise(front);
+
+  lv_obj_t *tap = lv_obj_create(s_body);
+  lv_obj_remove_flag(tap, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_remove_flag(tap, LV_OBJ_FLAG_CLICKABLE);
+  lv_obj_set_size(tap, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+  lv_obj_set_style_bg_opa(tap, LV_OPA_TRANSP, 0);
+  lv_obj_set_style_border_width(tap, 0, 0);
+  lv_obj_set_style_pad_all(tap, 0, 0);
+  lv_obj_set_style_pad_column(tap, 7, 0);
+  lv_obj_set_flex_flow(tap, LV_FLEX_FLOW_ROW);
+  lv_obj_set_flex_align(tap, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+  lv_obj_align(tap, LV_ALIGN_CENTER, 0, WALLET_TAP_Y);
+
+  lv_obj_t *dot = lv_obj_create(tap);
+  lv_obj_remove_flag(dot, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_size(dot, 8, 8);
+  lv_obj_set_style_radius(dot, LV_RADIUS_CIRCLE, 0);
+  lv_obj_set_style_border_width(dot, 0, 0);
+  lv_obj_set_style_bg_color(dot, lv_color_hex(SIG_GREEN), 0);
+  lv_obj_set_style_bg_opa(dot, LV_OPA_COVER, 0);
+  blink_loop(dot, 900, 0);
+
+  lv_obj_t *txt = lv_label_create(tap);
+  lv_label_set_text(txt, "tap to broadcast");
+  lv_obj_set_style_text_font(txt, &lv_font_montserrat_12, 0);
+  lv_obj_set_style_text_color(txt, lv_color_hex(SIG_GREEN), 0);
+
+  s_dots = page_dots_create(s_body, s_count, LV_ALIGN_BOTTOM_MID, 0, -4);
+  page_dots_set(&s_dots, s_sel);
+}
 
 static void overlay_close(void) {
   if (s_ov) {
@@ -54,32 +235,64 @@ static void overlay_close(void) {
   s_em = EM_NONE;
 }
 
-static void overlay_start(const char *name) {
+static void overlay_start(const nfc_sim_card_t *c) {
+  s_card = *c;
+
   s_ov = lv_obj_create(s_screen);
   lv_obj_set_size(s_ov, lv_pct(100), lv_pct(100));
   lv_obj_center(s_ov);
   lv_obj_remove_flag(s_ov, LV_OBJ_FLAG_SCROLLABLE);
   lv_obj_set_style_bg_color(s_ov, lv_color_black(), 0);
-  lv_obj_set_style_bg_opa(s_ov, LV_OPA_90, 0);
+  lv_obj_set_style_bg_opa(s_ov, LV_OPA_COVER, 0);
   lv_obj_set_style_border_width(s_ov, 0, 0);
+  lv_obj_set_style_pad_all(s_ov, 0, 0);
 
-  ui_chrome_header(s_ov, "EMULATE", "/assets/icons/nfc_icon.bin");
+  ui_chrome_header(s_ov, "EMULATE", CARD_ICON);
   ui_chrome_footer(s_ov, "BACK  Stop");
 
-  lv_obj_t *nm = lv_label_create(s_ov);
-  lv_obj_set_width(nm, lv_pct(86));
-  lv_label_set_text(nm, name);
-  lv_obj_set_style_text_color(nm, current_theme.text_main, 0);
-  lv_obj_set_style_text_font(nm, &lv_font_montserrat_14, 0);
-  lv_obj_set_style_text_align(nm, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_align(nm, LV_ALIGN_TOP_MID, 0, 52);
+  lv_obj_t *field_box = lv_obj_create(s_ov);
+  lv_obj_remove_flag(field_box, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_remove_flag(field_box, LV_OBJ_FLAG_CLICKABLE);
+  lv_obj_set_size(field_box, FIELD_BOX_W, FIELD_BOX_H);
+  lv_obj_align(field_box, LV_ALIGN_CENTER, 0, CARD_Y_OFS);
+  lv_obj_set_style_bg_opa(field_box, LV_OPA_TRANSP, 0);
+  lv_obj_set_style_border_width(field_box, 0, 0);
+  lv_obj_set_style_pad_all(field_box, 0, 0);
 
-  nfc_ui_field_create(&s_ov_field, s_ov, lv_color_hex(FIELD_GREEN));
+  nfc_ui_field_create(&s_ov_field, field_box, ui_theme_get_accent());
 
-  lv_obj_t *status = lv_label_create(s_ov);
-  lv_label_set_text(status, "Present to reader");
-  lv_obj_set_style_text_color(status, current_theme.text_main, 0);
-  lv_obj_align(status, LV_ALIGN_BOTTOM_MID, 0, -34);
+  lv_obj_t *card = nfc_ui_card_panel(s_ov, &s_card);
+  lv_obj_align(card, LV_ALIGN_CENTER, 0, CARD_Y_OFS);
+  lv_obj_fade_in(card, 280, 0);
+  card_rise(card);
+
+  lv_obj_t *bc = lv_label_create(s_ov);
+  lv_label_set_text(bc, "Broadcasting");
+  lv_obj_set_style_text_font(bc, &lv_font_montserrat_14, 0);
+  lv_obj_set_style_text_color(bc, current_theme.border_accent, 0);
+  lv_obj_align(bc, LV_ALIGN_BOTTOM_MID, 0, -46);
+  blink_loop(bc, 900, 0);
+
+  lv_obj_t *dots = lv_obj_create(s_ov);
+  lv_obj_remove_flag(dots, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_size(dots, 44, 12);
+  lv_obj_align(dots, LV_ALIGN_BOTTOM_MID, 0, -30);
+  lv_obj_set_style_bg_opa(dots, LV_OPA_TRANSP, 0);
+  lv_obj_set_style_border_width(dots, 0, 0);
+  lv_obj_set_style_pad_all(dots, 0, 0);
+  lv_obj_set_flex_flow(dots, LV_FLEX_FLOW_ROW);
+  lv_obj_set_flex_align(dots, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+  lv_obj_set_style_pad_column(dots, 6, 0);
+  for (int i = 0; i < TX_DOTS; i++) {
+    lv_obj_t *d = lv_obj_create(dots);
+    lv_obj_remove_flag(d, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_size(d, 6, 6);
+    lv_obj_set_style_radius(d, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_style_border_width(d, 0, 0);
+    lv_obj_set_style_bg_color(d, current_theme.border_accent, 0);
+    lv_obj_set_style_bg_opa(d, LV_OPA_COVER, 0);
+    blink_loop(d, 900, (int32_t)i * 300);
+  }
 
   s_em = EM_RUN;
   s_em_start = lv_tick_get();
@@ -97,9 +310,11 @@ static void nav_timer_cb(lv_timer_t *t) {
   bool ok = ok_button_is_down(), back = back_button_is_down();
 
   if (s_em == EM_RUN) {
-    nfc_ui_field_tick(&s_ov_field, lv_tick_get() - s_em_start);
-    if (back && !s_back_last)
-      overlay_close();
+    if (!(msgbox_is_open() || keyboard_is_open() || ui_input_is_locked())) {
+      nfc_ui_field_tick(&s_ov_field, lv_tick_get() - s_em_start);
+      if (back && !s_back_last)
+        overlay_close();
+    }
     s_up_last = up;
     s_down_last = down;
     s_left_last = left;
@@ -109,7 +324,7 @@ static void nav_timer_cb(lv_timer_t *t) {
     return;
   }
 
-  if (ui_input_is_locked()) {
+  if (ui_input_is_locked() || msgbox_is_open() || keyboard_is_open()) {
     s_up_last = up;
     s_down_last = down;
     s_left_last = left;
@@ -125,15 +340,20 @@ static void nav_timer_cb(lv_timer_t *t) {
   }
 
   if (!s_empty) {
-    if (down && !s_down_last)
-      menu_component_next(&s_menu);
-    if (up && !s_up_last)
-      menu_component_prev(&s_menu);
+    if (down && !s_down_last) {
+      s_sel = (s_sel + 1) % s_count;
+      wallet_build();
+      ui_feedback(UI_FB_NAV);
+    }
+    if (up && !s_up_last) {
+      s_sel = (s_sel == 0) ? s_count - 1 : s_sel - 1;
+      wallet_build();
+      ui_feedback(UI_FB_NAV);
+    }
     if ((ok && !s_ok_last) || (right && !s_right_last)) {
-      int sel = menu_component_get_selected(&s_menu);
-      const nfc_sim_card_t *c = nfc_sim_saved_get(sel);
+      const nfc_sim_card_t *c = nfc_sim_saved_get(s_sel);
       if (c != NULL)
-        overlay_start(c->name);
+        overlay_start(c);
     }
   }
 
@@ -152,6 +372,9 @@ void ui_nfc_emulate_open(void) {
     s_screen = NULL;
   }
   s_ov = NULL;
+  s_body = NULL;
+  s_sel = 0;
+  s_count = 0;
   for (int i = 0; i < 3; i++)
     s_ov_field.ring[i] = NULL;
   s_em = EM_NONE;
@@ -166,18 +389,15 @@ void ui_nfc_emulate_open(void) {
   s_empty = (n == 0);
 
   if (s_empty) {
-    ui_chrome_header(s_screen, "EMULATE", "/assets/icons/emulate_icon.bin");
+    ui_chrome_header(s_screen, "EMULATE", CARD_ICON);
     ui_chrome_footer(s_screen, "BACK  Back");
-    lv_obj_t *msg = lv_label_create(s_screen);
-    lv_label_set_text(msg, "No cards to emulate.\nRead a tag first.");
-    lv_obj_set_style_text_color(msg, current_theme.text_main, 0);
-    lv_obj_set_style_text_align(msg, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_center(msg);
+    build_empty(CARD_ICON, "Nothing to emulate", "Read a tag first");
   } else {
-    s_menu = menu_component_create(s_screen, "Emulate", CARD_ICON);
-    int cap = n > 10 ? 10 : n;
-    for (int i = 0; i < cap; i++)
-      menu_component_add_item(&s_menu, CARD_ICON, nfc_sim_saved_get(i)->name);
+    ui_chrome_header(s_screen, "EMULATE", CARD_ICON);
+    ui_chrome_footer(s_screen, LV_SYMBOL_UP LV_SYMBOL_DOWN " Flip   OK Emulate   BACK Exit");
+    s_count = n > MAX_CARDS ? MAX_CARDS : n;
+    s_sel = 0;
+    wallet_build();
   }
 
   if (s_nav_timer == NULL)
--- firmware_p4/components/Applications/ui/screens/nfc/nfc_saved_ui.c
+++ firmware_p4/components/Applications/ui/screens/nfc/nfc_saved_ui.c
@@ -15,32 +15,56 @@
 
 #include "nfc_saved_ui.h"
 
+#include <stdio.h>
+
 #include "lvgl.h"
 
+#include "st7789.h"
+
+#include "assets_manager.h"
 #include "buttons_gpio.h"
+#include "capture_result_ui.h"
+#include "msgbox_ui.h"
 #include "nfc_sim.h"
 #include "nfc_ui_common.h"
+#include "notify_ui.h"
+#include "page_dots_ui.h"
 #include "ui_chrome.h"
+#include "ui_feedback.h"
 #include "ui_manager.h"
 #include "ui_theme.h"
 
-#define NAV_TIMER_MS 50
-#define CARD_H       122
-#define PEEK         60
+#define NAV_TIMER_MS   50
+#define CARD_H         122
+#define PEEK           60
+#define COL_DIM        0x8A8594
+#define SAVED_ICON     "/assets/icons/bookmarks.bin"
+#define CARD_ICON      "/assets/icons/nfc.bin"
+#define CARD_REVEAL_MS 1100
+#define UID_VAL_LEN    40
 
 enum { DT_NONE, DT_VIEW };
+enum { DV_CARD, DV_CHOICES };
 
 static lv_obj_t *s_screen = NULL;
 static lv_obj_t *s_cont = NULL;
 static lv_timer_t *s_nav_timer = NULL;
 static lv_obj_t *s_cards[NFC_SIM_MAX_SAVED];
+static page_dots_t s_dots;
+static bool s_has_dots = false;
 static int s_count = 0;
 static int s_sel = 0;
 static bool s_empty = false;
 
 static lv_obj_t *s_ov = NULL;
+static lv_obj_t *s_ov_card = NULL;
+static lv_obj_t *s_ov_footer = NULL;
+static capture_result_t s_cr = {0};
+static nfc_sim_card_t s_view_card;
 static int s_detail = DT_NONE;
+static int s_phase = DV_CARD;
 static int s_sel_idx = -1;
+static uint32_t s_reveal_start = 0;
 
 static bool s_up_last, s_down_last, s_ok_last, s_back_last, s_left_last, s_right_last;
 
@@ -49,18 +73,77 @@ static void rebuild_async(void *p) {
   ui_nfc_saved_open();
 }
 
+static lv_obj_t *lit_panel(lv_obj_t *parent, int w, int h) {
+  lv_obj_t *p = lv_obj_create(parent);
+  lv_obj_remove_flag(p, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_size(p, w, h);
+  lv_obj_set_style_radius(p, 13, 0);
+  lv_obj_set_style_bg_color(p, current_theme.bg_secondary, 0);
+  lv_obj_set_style_bg_opa(p, LV_OPA_COVER, 0);
+  lv_obj_set_style_bg_grad_dir(p, LV_GRAD_DIR_NONE, 0);
+  lv_obj_set_style_border_width(p, 1, 0);
+  lv_obj_set_style_border_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_shadow_color(p, current_theme.border_accent, 0);
+  lv_obj_set_style_shadow_width(p, 16, 0);
+  lv_obj_set_style_shadow_opa(p, LV_OPA_40, 0);
+  lv_obj_set_style_shadow_spread(p, -4, 0);
+  return p;
+}
+
+static void build_empty(void) {
+  lv_obj_t *card = lit_panel(s_screen, 200, 104);
+  lv_obj_align(card, LV_ALIGN_CENTER, 0, 6);
+  lv_obj_set_style_pad_all(card, 10, 0);
+  lv_obj_set_flex_flow(card, LV_FLEX_FLOW_COLUMN);
+  lv_obj_set_flex_align(card, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+  lv_obj_set_style_pad_row(card, 6, 0);
+
+  lv_image_dsc_t *dsc = assets_get(SAVED_ICON);
+  if (dsc != NULL) {
+    lv_obj_t *img = lv_image_create(card);
+    lv_image_set_src(img, dsc);
+    lv_obj_set_style_image_recolor(img, current_theme.text_main, 0);
+    lv_obj_set_style_image_recolor_opa(img, LV_OPA_COVER, 0);
+  }
+  lv_obj_t *t = lv_label_create(card);
+  lv_label_set_text(t, "No saved cards");
+  lv_obj_set_style_text_font(t, &lv_font_montserrat_14, 0);
+  lv_obj_set_style_text_color(t, current_theme.text_main, 0);
+  lv_obj_t *s = lv_label_create(card);
+  lv_label_set_text(s, "Read a tag first");
+  lv_obj_set_style_text_font(s, &lv_font_montserrat_12, 0);
+  lv_obj_set_style_text_color(s, lv_color_hex(COL_DIM), 0);
+}
+
 static void relayout(void) {
   for (int i = 0; i < s_count; i++) {
     int y = i * PEEK;
     if (i > s_sel)
       y += (CARD_H - PEEK);
     lv_obj_align(s_cards[i], LV_ALIGN_TOP_MID, 0, y);
-    lv_obj_set_style_border_width(s_cards[i], (i == s_sel) ? 3 : 1, 0);
+
+    bool sel = (i == s_sel);
+    if (sel) {
+      lv_obj_set_style_border_width(s_cards[i], 3, 0);
+      lv_obj_set_style_border_color(s_cards[i], current_theme.border_accent, 0);
+      lv_obj_set_style_bg_opa(s_cards[i], LV_OPA_COVER, 0);
+      lv_obj_set_style_shadow_color(s_cards[i], current_theme.border_accent, 0);
+      lv_obj_set_style_shadow_width(s_cards[i], 14, 0);
+      lv_obj_set_style_shadow_opa(s_cards[i], LV_OPA_50, 0);
+      lv_obj_set_style_shadow_spread(s_cards[i], -3, 0);
+    } else {
+      lv_obj_set_style_border_width(s_cards[i], 1, 0);
+      lv_obj_set_style_border_color(s_cards[i], nfc_ui_card_color(nfc_sim_saved_get(i)), 0);
+      lv_obj_set_style_shadow_width(s_cards[i], 0, 0);
+      lv_obj_set_style_shadow_opa(s_cards[i], LV_OPA_TRANSP, 0);
+    }
   }
   int sy = s_sel * PEEK - 6;
   if (sy < 0)
     sy = 0;
   lv_obj_scroll_to_y(s_cont, sy, LV_ANIM_OFF);
+  if (s_has_dots)
+    page_dots_set(&s_dots, s_sel);
 }
 
 static void overlay_close(void) {
@@ -68,31 +151,84 @@ static void overlay_close(void) {
     lv_obj_del(s_ov);
     s_ov = NULL;
   }
+  s_cr = (capture_result_t){0};
+  s_ov_card = NULL;
+  s_ov_footer = NULL;
   s_detail = DT_NONE;
+  s_phase = DV_CARD;
 }
 
 static void overlay_open(const nfc_sim_card_t *c, int idx) {
+  s_view_card = *c;
+
   s_ov = lv_obj_create(s_screen);
   lv_obj_set_size(s_ov, lv_pct(100), lv_pct(100));
   lv_obj_center(s_ov);
   lv_obj_remove_flag(s_ov, LV_OBJ_FLAG_SCROLLABLE);
   lv_obj_set_style_bg_color(s_ov, lv_color_black(), 0);
-  lv_obj_set_style_bg_opa(s_ov, LV_OPA_80, 0);
+  lv_obj_set_style_bg_opa(s_ov, LV_OPA_90, 0);
   lv_obj_set_style_border_width(s_ov, 0, 0);
+  lv_obj_set_style_pad_all(s_ov, 0, 0);
 
-  lv_obj_t *panel = nfc_ui_card_panel(s_ov, c);
-  lv_obj_align(panel, LV_ALIGN_CENTER, 0, -6);
+  ui_chrome_header(s_ov, "CARD", SAVED_ICON);
+  s_ov_footer = ui_chrome_footer(s_ov, "OK  Options    BACK  Back");
 
-  lv_obj_t *hint = lv_label_create(s_ov);
-  lv_label_set_text(hint, LV_SYMBOL_TRASH "  OK = Delete    BACK = Back");
-  lv_obj_set_style_text_color(hint, current_theme.text_main, 0);
-  lv_obj_set_style_text_opa(hint, LV_OPA_70, 0);
-  lv_obj_set_style_text_font(hint, &lv_font_montserrat_12, 0);
-  lv_obj_align(hint, LV_ALIGN_BOTTOM_MID, 0, -10);
+  s_ov_card = nfc_ui_card_panel(s_ov, &s_view_card);
+  lv_obj_align(s_ov_card, LV_ALIGN_CENTER, 0, -6);
+  lv_obj_fade_in(s_ov_card, 220, 0);
 
-  lv_obj_fade_in(s_ov, 220, 0);
   s_detail = DT_VIEW;
+  s_phase = DV_CARD;
   s_sel_idx = idx;
+  s_reveal_start = lv_tick_get();
+  ui_feedback(UI_FB_SELECT);
+}
+
+static void choices_step(bool forward) {
+  do {
+    if (forward)
+      capture_result_next(&s_cr);
+    else
+      capture_result_prev(&s_cr);
+  } while (capture_result_selected(&s_cr) != CAP_ACT_PRIMARY &&
+           capture_result_selected(&s_cr) != CAP_ACT_DISCARD);
+}
+
+static void present_choices(void) {
+  if (s_ov_card) {
+    lv_obj_del(s_ov_card);
+    s_ov_card = NULL;
+  }
+
+  char uid[24];
+  static char valbuf[UID_VAL_LEN];
+  nfc_sim_format_uid(&s_view_card, uid, sizeof(uid));
+  snprintf(valbuf, sizeof(valbuf), "UID %s", uid);
+
+  capture_result_cfg_t cfg = {
+      .accent = current_theme.border_accent,
+      .card_icon = CARD_ICON,
+      .card_title = s_view_card.name[0] ? s_view_card.name : s_view_card.type,
+      .card_sub = s_view_card.type,
+      .card_value = valbuf,
+      .primary_label = "Emulate",
+  };
+  s_cr = capture_result_create(s_ov, &cfg);
+  lv_obj_add_flag(s_cr.rows[CAP_ACT_SAVE], LV_OBJ_FLAG_HIDDEN);
+  lv_obj_add_flag(s_cr.rows[CAP_ACT_AGAIN], LV_OBJ_FLAG_HIDDEN);
+
+  ui_chrome_footer_set_text(s_ov_footer, LV_SYMBOL_UP LV_SYMBOL_DOWN " Choose   OK Do   BACK Back");
+  s_phase = DV_CHOICES;
+}
+
+static void on_del_confirm(bool confirm) {
+  if (!confirm)
+    return;
+  nfc_sim_remove(s_sel_idx);
+  ui_feedback(UI_FB_WRITE);
+  notify(NOTIFY_INFO, "Card deleted");
+  overlay_close();
+  lv_async_call(rebuild_async, NULL);
 }
 
 static void nav_timer_cb(lv_timer_t *t) {
@@ -106,17 +242,33 @@ static void nav_timer_cb(lv_timer_t *t) {
   bool ok = ok_button_is_down(), back = back_button_is_down();
 
   if (s_detail == DT_VIEW) {
-    if (back && !s_back_last) {
-      overlay_close();
-    } else if (ok && !s_ok_last) {
-      nfc_sim_remove(s_sel_idx);
-      overlay_close();
-      lv_async_call(rebuild_async, NULL);
+    if (!(msgbox_is_open() || ui_input_is_locked())) {
+      if (back && !s_back_last) {
+        overlay_close();
+      } else if (s_phase == DV_CARD) {
+        if ((ok && !s_ok_last) || (lv_tick_get() - s_reveal_start >= CARD_REVEAL_MS))
+          present_choices();
+      } else {
+        if (down && !s_down_last) {
+          choices_step(true);
+          ui_feedback(UI_FB_NAV);
+        } else if (up && !s_up_last) {
+          choices_step(false);
+          ui_feedback(UI_FB_NAV);
+        } else if (ok && !s_ok_last) {
+          if (capture_result_selected(&s_cr) == CAP_ACT_PRIMARY) {
+            ui_feedback(UI_FB_EMULATE);
+            ui_switch_screen(SCREEN_NFC_EMULATE);
+            return;
+          }
+          msgbox_open(LV_SYMBOL_TRASH, "Delete this card?", "Delete", "Cancel", on_del_confirm);
+        }
+      }
     }
     goto save;
   }
 
-  if (ui_input_is_locked())
+  if (ui_input_is_locked() || msgbox_is_open())
     goto save;
 
   if ((back && !s_back_last) || (left && !s_left_last)) {
@@ -128,10 +280,12 @@ static void nav_timer_cb(lv_timer_t *t) {
     if (down && !s_down_last && s_sel < s_count - 1) {
       s_sel++;
       relayout();
+      ui_feedback(UI_FB_NAV);
     }
     if (up && !s_up_last && s_sel > 0) {
       s_sel--;
       relayout();
+      ui_feedback(UI_FB_NAV);
     }
     if ((ok && !s_ok_last) || (right && !s_right_last)) {
       const nfc_sim_card_t *c = nfc_sim_saved_get(s_sel);
@@ -160,7 +314,12 @@ void ui_nfc_saved_open(void) {
     s_screen = NULL;
   }
   s_ov = NULL;
+  s_ov_card = NULL;
+  s_ov_footer = NULL;
+  s_cr = (capture_result_t){0};
+  s_has_dots = false;
   s_detail = DT_NONE;
+  s_phase = DV_CARD;
   s_sel_idx = -1;
   s_sel = 0;
   s_up_last = s_down_last = s_ok_last = s_back_last = s_left_last = s_right_last = false;
@@ -170,7 +329,7 @@ void ui_nfc_saved_open(void) {
   lv_obj_set_style_bg_opa(s_screen, LV_OPA_COVER, 0);
   lv_obj_remove_flag(s_screen, LV_OBJ_FLAG_SCROLLABLE);
 
-  ui_chrome_header(s_screen, "SAVED", "/assets/icons/card_icon.bin");
+  ui_chrome_header(s_screen, "SAVED", SAVED_ICON);
 
   s_count = nfc_sim_saved_count();
   if (s_count > NFC_SIM_MAX_SAVED)
@@ -178,18 +337,12 @@ void ui_nfc_saved_open(void) {
   s_empty = (s_count == 0);
 
   if (s_empty) {
-    lv_obj_t *msg = lv_label_create(s_screen);
-    lv_label_set_text(msg, "No saved cards.\nRead a tag first.");
-    lv_obj_set_style_text_color(msg, current_theme.text_main, 0);
-    lv_obj_set_style_text_align(msg, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_center(msg);
+    build_empty();
+    ui_chrome_footer(s_screen, "BACK  Back");
   } else {
-    int H = lv_display_get_vertical_resolution(NULL);
-    if (H < 200)
-      H = 320;
     s_cont = lv_obj_create(s_screen);
-    lv_obj_set_size(s_cont, lv_pct(100), H - 46 - 26);
-    lv_obj_align(s_cont, LV_ALIGN_TOP_MID, 0, 46);
+    lv_obj_set_size(s_cont, lv_pct(100), LCD_V_RES - UI_CHROME_HEADER_H - UI_CHROME_FOOTER_H);
+    lv_obj_align(s_cont, LV_ALIGN_TOP_MID, 0, UI_CHROME_HEADER_H);
     lv_obj_set_style_bg_opa(s_cont, LV_OPA_TRANSP, 0);
     lv_obj_set_style_border_width(s_cont, 0, 0);
     lv_obj_set_style_pad_all(s_cont, 0, 0);
@@ -197,11 +350,14 @@ void ui_nfc_saved_open(void) {
 
     for (int i = 0; i < s_count; i++) {
       s_cards[i] = nfc_ui_card_panel(s_cont, nfc_sim_saved_get(i));
-      lv_obj_set_style_shadow_width(s_cards[i], 0, 0);
       lv_obj_set_style_bg_grad_dir(s_cards[i], LV_GRAD_DIR_NONE, 0);
     }
     lv_obj_update_layout(s_screen);
     relayout();
+
+    s_dots = page_dots_create(s_screen, s_count, LV_ALIGN_BOTTOM_MID, 0, -28);
+    s_has_dots = true;
+    page_dots_set(&s_dots, s_sel);
 
     ui_chrome_footer(s_screen, LV_SYMBOL_UP LV_SYMBOL_DOWN " Browse   OK Open   BACK Exit");
   }
