05676e773d1992a6c726453595bd9bf4886ca423
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-08-14 22:17:19 -0300

    refactor(ui): migrate nfc ndef to event-driven input

--- firmware_p4/components/Applications/ui/screens/nfc/nfc_ndef_ui.c
+++ firmware_p4/components/Applications/ui/screens/nfc/nfc_ndef_ui.c
@@ -18,13 +18,10 @@
 #include "lvgl.h"
 #include "st7789.h"
 
-#include "buttons_gpio.h"
 #include "ui_chrome.h"
 #include "ui_feedback.h"
 #include "ui_manager.h"
 #include "ui_theme.h"
-
-#define NAV_TIMER_MS 50
 
 #define MX        8
 #define BODY_H    (LCD_V_RES - UI_CHROME_HEADER_H - UI_CHROME_FOOTER_H)
@@ -66,14 +63,8 @@ static lv_obj_t *s_screen = NULL;
 static lv_obj_t *s_row[REC_COUNT];
 static lv_obj_t *s_icon[REC_COUNT];
 static lv_obj_t *s_title[REC_COUNT];
-static lv_timer_t *s_nav_timer = NULL;
 
 static int s_sel = 0;
-static bool s_ok_last = false;
-static bool s_back_last = false;
-static bool s_left_last = false;
-static bool s_up_last = false;
-static bool s_down_last = false;
 
 static void make_chip(lv_obj_t *parent, const char *txt, bool sel) {
   lv_obj_t *chip = lv_obj_create(parent);
@@ -181,43 +172,38 @@ static void build_body(lv_obj_t *parent) {
   refresh_selection();
 }
 
-static void nav_timer_cb(lv_timer_t *t) {
-  if (lv_screen_active() != s_screen) {
-    lv_timer_delete(t);
-    s_nav_timer = NULL;
-    return;
-  }
-  if (ui_input_is_locked())
-    return;
+static void nfc_ndef_input(const input_event_t *ev, void *ctx) {
+  (void)ctx;
+  const bool press = (ev->action == INPUT_ACTION_PRESS);
+  const bool nav = press || (ev->action == INPUT_ACTION_REPEAT);
 
-  bool ok = ok_button_is_down();
-  bool back = back_button_is_down();
-  bool left = ui_btn_left();
-  bool up = ui_btn_up();
-  bool down = ui_btn_down();
-
-  if ((back && !s_back_last) || (left && !s_left_last)) {
-    ui_switch_screen(SCREEN_NFC_MENU);
-    return;
+  switch (ev->button) {
+    case INPUT_BTN_BACK:
+    case INPUT_BTN_LEFT:
+      if (press)
+        ui_switch_screen(SCREEN_NFC_MENU);
+      break;
+    case INPUT_BTN_DOWN:
+      if (nav) {
+        s_sel = (s_sel + 1) % REC_COUNT;
+        refresh_selection();
+        ui_feedback(UI_FB_NAV);
+      }
+      break;
+    case INPUT_BTN_UP:
+      if (nav) {
+        s_sel = (s_sel - 1 + REC_COUNT) % REC_COUNT;
+        refresh_selection();
+        ui_feedback(UI_FB_NAV);
+      }
+      break;
+    case INPUT_BTN_OK:
+      if (press)
+        ui_feedback(UI_FB_SELECT);
+      break;
+    default:
+      break;
   }
-  if (down && !s_down_last) {
-    s_sel = (s_sel + 1) % REC_COUNT;
-    refresh_selection();
-    ui_feedback(UI_FB_NAV);
-  }
-  if (up && !s_up_last) {
-    s_sel = (s_sel - 1 + REC_COUNT) % REC_COUNT;
-    refresh_selection();
-    ui_feedback(UI_FB_NAV);
-  }
-  if (ok && !s_ok_last)
-    ui_feedback(UI_FB_SELECT);
-
-  s_ok_last = ok;
-  s_back_last = back;
-  s_left_last = left;
-  s_up_last = up;
-  s_down_last = down;
 }
 
 void ui_nfc_ndef_open(void) {
@@ -226,7 +212,6 @@ void ui_nfc_ndef_open(void) {
     s_screen = NULL;
   }
   s_sel = 0;
-  s_ok_last = s_back_last = s_left_last = s_up_last = s_down_last = false;
 
   s_screen = lv_obj_create(NULL);
   lv_obj_set_style_bg_color(s_screen, current_theme.screen_base, 0);
@@ -252,8 +237,7 @@ void ui_nfc_ndef_open(void) {
 
   ui_chrome_footer(s_screen, FOOTER_HINT);
 
-  if (s_nav_timer == NULL)
-    s_nav_timer = lv_timer_create(nav_timer_cb, NAV_TIMER_MS, NULL);
+  ui_input_set_screen_handler(nfc_ndef_input, NULL);
 
   ui_screen_load(s_screen);
 }
