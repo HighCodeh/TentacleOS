0b3c8c8258c49bed514343a0cb78e7994f587361
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-08-14 22:17:20 -0300

    refactor(ui): migrate nfc read to event-driven input

--- firmware_p4/components/Applications/ui/screens/nfc/nfc_read_ui.c
+++ firmware_p4/components/Applications/ui/screens/nfc/nfc_read_ui.c
@@ -20,19 +20,17 @@
 #include "esp_random.h"
 #include "lvgl.h"
 
-#include "buttons_gpio.h"
 #include "capture_result_ui.h"
 #include "keyboard_ui.h"
 #include "notify_ui.h"
 #include "ui_feedback.h"
-#include "msgbox_ui.h"
 #include "nfc_sim.h"
 #include "nfc_ui_common.h"
 #include "ui_chrome.h"
 #include "ui_manager.h"
 #include "ui_theme.h"
 
-#define NAV_TIMER_MS 33
+#define REFRESH_MS   33
 #define REVEAL_MS    3000
 
 #define COL_DIM      0x8A8594
@@ -63,8 +61,6 @@ static uint32_t s_scan_start = 0;
 static uint32_t s_scan_deadline = 1800;
 static uint32_t s_found_at = 0;
 static nfc_sim_card_t s_card;
-
-static bool s_ok_last, s_back_last, s_right_last, s_up_last, s_down_last;
 
 static void show_rings(bool show) {
   for (int i = 0; i < 3; i++) {
@@ -177,28 +173,62 @@ static void on_name_submit(const char *text, void *ud) {
   }
 }
 
-static void tick_cb(lv_timer_t *t) {
+static void nfc_read_input(const input_event_t *ev, void *ctx) {
+  (void)ctx;
+  const bool press = (ev->action == INPUT_ACTION_PRESS);
+  const bool nav = press || (ev->action == INPUT_ACTION_REPEAT);
+
+  if (ev->button == INPUT_BTN_BACK) {
+    if (press)
+      ui_switch_screen(SCREEN_NFC_MENU);
+    return;
+  }
+
+  if (s_state != ST_OPTIONS)
+    return;
+
+  switch (ev->button) {
+    case INPUT_BTN_DOWN:
+      if (nav) {
+        capture_result_next(&s_cr);
+        ui_feedback(UI_FB_NAV);
+      }
+      break;
+    case INPUT_BTN_UP:
+      if (nav) {
+        capture_result_prev(&s_cr);
+        ui_feedback(UI_FB_NAV);
+      }
+      break;
+    case INPUT_BTN_OK:
+      if (press) {
+        switch (capture_result_selected(&s_cr)) {
+          case CAP_ACT_PRIMARY:
+            ui_switch_screen(SCREEN_NFC_EMULATE);
+            break;
+          case CAP_ACT_SAVE:
+            keyboard_open(NULL, on_name_submit, NULL);
+            break;
+          case CAP_ACT_AGAIN:
+            begin_scan();
+            break;
+          case CAP_ACT_DISCARD:
+            ui_switch_screen(SCREEN_NFC_MENU);
+            break;
+          default:
+            break;
+        }
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+static void refresh_cb(lv_timer_t *t) {
   if (lv_screen_active() != s_screen) {
     lv_timer_delete(t);
     s_timer = NULL;
-    return;
-  }
-  bool ok = ok_button_is_down();
-  bool back = back_button_is_down();
-  bool right = ui_btn_right();
-  bool up = ui_btn_up();
-  bool down = ui_btn_down();
-
-  if (msgbox_is_open() || keyboard_is_open() || ui_input_is_locked()) {
-    s_ok_last = ok;
-    s_back_last = back;
-    s_right_last = right;
-    s_up_last = up;
-    s_down_last = down;
-    return;
-  }
-  if (back && !s_back_last) {
-    ui_switch_screen(SCREEN_NFC_MENU);
     return;
   }
 
@@ -220,40 +250,7 @@ static void tick_cb(lv_timer_t *t) {
   } else if (s_state == ST_FOUND) {
     if (lv_tick_get() - s_found_at >= REVEAL_MS)
       show_options();
-  } else {
-    if (down && !s_down_last) {
-      capture_result_next(&s_cr);
-      ui_feedback(UI_FB_NAV);
-    }
-    if (up && !s_up_last) {
-      capture_result_prev(&s_cr);
-      ui_feedback(UI_FB_NAV);
-    }
-    if (ok && !s_ok_last) {
-      switch (capture_result_selected(&s_cr)) {
-        case CAP_ACT_PRIMARY:
-          ui_switch_screen(SCREEN_NFC_EMULATE);
-          return;
-        case CAP_ACT_SAVE:
-          keyboard_open(NULL, on_name_submit, NULL);
-          break;
-        case CAP_ACT_AGAIN:
-          begin_scan();
-          break;
-        case CAP_ACT_DISCARD:
-          ui_switch_screen(SCREEN_NFC_MENU);
-          return;
-        default:
-          break;
-      }
-    }
   }
-
-  s_ok_last = ok;
-  s_back_last = back;
-  s_right_last = right;
-  s_up_last = up;
-  s_down_last = down;
 }
 
 void ui_nfc_read_open(void) {
@@ -266,7 +263,6 @@ void ui_nfc_read_open(void) {
   s_dump = NULL;
   s_cr = (capture_result_t){0};
   s_found_at = 0;
-  s_ok_last = s_back_last = s_right_last = s_up_last = s_down_last = false;
 
   s_screen = lv_obj_create(NULL);
   lv_obj_set_style_bg_color(s_screen, current_theme.screen_base, 0);
@@ -286,7 +282,9 @@ void ui_nfc_read_open(void) {
   begin_scan();
 
   if (s_timer == NULL)
-    s_timer = lv_timer_create(tick_cb, NAV_TIMER_MS, NULL);
+    s_timer = lv_timer_create(refresh_cb, REFRESH_MS, NULL);
+
+  ui_input_set_screen_handler(nfc_read_input, NULL);
 
   ui_screen_load(s_screen);
 }
